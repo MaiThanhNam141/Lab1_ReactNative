ca55bd74d4256251904b116b462a09e3d809d0d7
MaiThanhNam141 <maithanhnam141@gmail.com>
2023-09-20 09:56:35 +0700

    Change something

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, Text, View } from 'react-native';
-// import HomeScreen from './src/HomeScreen';
-// import DetailsScreen from './src/Detail';
-// import Lab1 from './src/Lab1';
-// import Lab2 from './src/Lab2';
-// import Lab3 from './src/Lab3';
-// import Lab4 from './src/Lab4';
-// import Lab5 from './src/Lab5';
-// import Lab6 from './src/Lab6';
-// import Lab7 from './src/Lab7';
-// import Lab8 from './src/Lab8';
-// import Calyculator from './src/Calyculator'
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       {/* <Lab1 />
-//       <Lab2 />
-//       <Lab3 /> 
-//       <Lab4 />
-//       <Lab5 />
-//       <Lab6 />
-//       <Lab7 />
-//       <Lab8 /> 
-//       <Calyculator></Calyculator>*/}
-
-
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+import Lab1 from './src/Lab1';
+import Lab2 from './src/Lab2';
+import Lab3 from './src/Lab3';
+import Lab4 from './src/Lab4';
+import Lab5 from './src/Lab5';
+import Lab6 from './src/Lab6';
+import Lab7 from './src/Lab7';
+import Lab8 from './src/Lab8';
+import Calyculator from './src/Calyculator'
 import React from 'react';
 import HomeScreen from './src/HomeScreen';
 import Detail from './src/Detail';
@@ -49,7 +17,6 @@ import { Image, TouchableOpacity } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
-
 const App = () => {
   return (
     <PaperProvider>
@@ -65,17 +32,18 @@ const App = () => {
                     <Image source={require('./CustomLib/arrowLeft.png')} style={{ width: 24, height: 24}} />
                   </TouchableOpacity>
                 );
-              } else {
-                return null; // Không hiển thị mũi tên quay lại trên màn hình Home
-              }
+              } else return null
             },
           })}
         >
           <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'HomeScreen' }}/>
           <Stack.Screen name="Detail" component={Detail} options={{ title: 'Detail' }}/> 
         </Stack.Navigator>
+        
       </NavigationContainer>
+      
     </PaperProvider>
+    
   );
 }
 export default App;
